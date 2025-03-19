454970181b804ae53f0c89cc1c7a5bbfbb2fec53
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-19 15:56:40 +0100

    Update

--- app/contacts/[contactId]/loading.tsx
+++ app/contacts/[contactId]/loading.tsx
@@ -5,6 +5,7 @@ export default function Loading() {
   return (
     <div className="flex flex-col gap-4 lg:flex-row">
       <div className="mr-8 h-[192px] w-[192px] rounded-3xl bg-gray" />
+
       <Skeleton />
     </div>
   );

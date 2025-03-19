8baddc7bde2535871f7b31ed4009cadcf39f2099
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-19 15:25:04 +0100

    Update

---
+++ data/services/getContact.ts
@@ -0,0 +1,17 @@
+import 'server-only';
+
+import { notFound } from 'next/navigation';
+import { prisma } from '@/db';
+
+export async function getContact(contactId: string) {
+  const contact = await prisma.contact.findUnique({
+    where: {
+      id: contactId,
+    },
+  });
+
+  if (!contact) {
+    notFound();
+  }
+  return contact;
+}

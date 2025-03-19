b9c5d77b276475e64a7eea1185b32f23fb21d2b8
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-19 14:51:09 +0100

    Update

--- app/(intro)/client-server/_components/ServerComponent.tsx
+++ app/(intro)/client-server/_components/ServerComponent.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { prisma } from '@/db';
 
-export default function ServerComponent() {
-  return <div className="rounded border-2 border-red-500 p-4">ServerComponent</div>;
+export default async function ServerComponent() {
+  // We can access our database here because we are on the server
+  const data = await prisma.contact.findMany();
+
+  return (
+    <div className="rounded border-2 border-red-500 p-4">
+      {data[0].first}
+      ServerComponent
+    </div>
+  );
 }

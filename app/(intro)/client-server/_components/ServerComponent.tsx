a585e672a97ba2e5dc2af0ffe64b6a978d9ce090
Aurora Scharff <aurora.sofie@gmail.com>
2025-03-19 14:47:27 +0100

    Update

---
+++ app/(intro)/client-server/_components/ServerComponent.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ServerComponent() {
+  return <div className="rounded border-2 border-red-500 p-4">ServerComponent</div>;
+}
---
+++ app/(intro)/client-server/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ServerComponent from './_components/ServerComponent';
+
+export default function ClientServerPage() {
+  return (
+    <div>
+      ClientServerPage
+      <ServerComponent />
+    </div>
+  );
+}
